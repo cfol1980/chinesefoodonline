9b597dd75782753f50c28e4c17c570c4a93ba1ea
cfol1980 <cfol19800701@gmail.com>
2025-08-09 10:33:20 -0400

    2025-08-09 supporter page

--- app/[supporter-id]/page.tsx
+++ app/[supporter-id]/page.tsx
@@ -36,7 +36,7 @@ export default function Supporter() {
   return (
     <div className="bg-gray-100 min-h-screen p-4">
       <h1 className="text-2xl font-bold text-center mb-4">Supporter: {supporterId}</h1>
-      <p className="text-lg">Welcome to your supporter page!</p>
+      <p className="text-lg">Welcome to {supporterId} page!</p>
     </div>
   );
 }
