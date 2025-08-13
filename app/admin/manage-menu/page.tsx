3304da9829ab2d2825ca47ee906d99be09a6a21a
cfol1980 <cfol19800701@gmail.com>
2025-08-13 19:29:12 -0400

    2025-08-13 admin menu

---
+++ app/admin/manage-menu/page.tsx
@@ -0,0 +1,9 @@
+export default function ManageMenuPage() {
+    return (
+      <div style={{ padding: '20px' }}>
+        <h1>Manage Menu</h1>
+        <p>This is where you’ll manage supporter menus in the future.</p>
+      </div>
+    );
+  }
+  
