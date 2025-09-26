4916fb58a8755f53ce767719daff9934c1c17146
cfol1980 <cfol19800701@gmail.com>
2025-09-26 08:45:24 -0400

    sfhh

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -170,6 +170,12 @@ export default function ManageUsersPage() {
                 </form>
               </td>
               <td className="py-2 px-4">
+              <Link
+    href={`/admin/supporters/${u.id}`}
+    className="px-3 py-1 bg-blue-500 text-white rounded hover:bg-blue-600"
+  >
+    Edit
+  </Link>
                 <button
                   onClick={() => handleDeleteUser(u.id)}
                   className="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600"
